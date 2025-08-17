8285e58fa0faf859c413b2b517092bec103a02f5
chong <c22425688@gmail.com>
2025-08-17 13:06:16 +0800

    xx

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import './assets/scss/all.scss';
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 2.5,
   spaceBetween: 24,
-  breakpoints: {
+    breakpoints: {
     // 手機 (0px 以上)
     0: {
-      slidesPerView: 1.1, // 一次只顯示 1 張
+      slidesPerView: 1.1, // 一次只顯示 1.1張
     },
     // 平板 (768px 以上)
     768: {
@@ -14,7 +14,7 @@ const swiper = new Swiper(".mySwiper", {
     },
     // 桌機 (992px 以上)
     992: {
-      slidesPerView: 3, // 一次顯示 3 張
+      slidesPerView: 2.5, // 一次顯示 2.5 張
     }
   }
 });
