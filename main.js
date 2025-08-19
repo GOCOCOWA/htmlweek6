8e2bbe4afe811e51ba1af5f3ebe1768fcf711765
chong <c22425688@gmail.com>
2025-08-19 11:49:38 +0800

    re

--- main.js
+++ main.js
@@ -18,6 +18,24 @@ const swiper = new Swiper(".mySwiper", {
     }
   }
 });
+const swiperm = new Swiper(".mySwiperm", {
+  slidesPerView: 2.5,
+  spaceBetween: 24,
+    breakpoints: {
+    // 手機 (0px 以上)
+    0: {
+      slidesPerView: 1.1, // 一次只顯示 1.1張
+    },
+    // 平板 (768px 以上)
+    768: {
+      slidesPerView: 2, // 一次顯示 2 張
+    },
+    // 桌機 (992px 以上)
+    992: {
+      slidesPerView: 3.8, // 一次顯示 3.8 張
+    }
+  }
+});
 const swipers = new Swiper(".mySwipers", {
   slidesPerView: 2.5,
   spaceBetween: 24,
